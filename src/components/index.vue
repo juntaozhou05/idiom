1b382a0b9d2608d6805b23e7a0888ee29f754cf6
350529307@qq.com <191136674@qq.com>
2017-04-14 13:06:16 +0800

    layout

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,8 +2,19 @@
   <div class="index">
     <div class="top">
       <img src="../assets/mainbg.png">
+      <img id="askImg" :src="askImg[0]" alt="">
+      <div class="btns">
+        <button id="reset">重来</button>
+        <button id="reminder">提示</button>
+      </div>
     </div>
-    <div></div>
+    <div>
+      
+    </div>
+    <ul>
+      <li></li>
+    </ul>
+    <button>提交</button>
   </div>
 </template>
 
@@ -11,7 +22,7 @@
 export default {
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      askImg: ['src/assets/imgs/1.png','../assets/mainbg.png']
     }
   }
 }
@@ -24,8 +35,29 @@ export default {
       width:90%;
       height:30%;
       margin:10px auto;
+      position: relative;
       img {
         width:100%;
+      }
+      .btns {
+        position: absolute;
+        top:160px;
+        left: 10%;
+        width: 80%;
+        text-align: center;
+        button {
+          padding:5px 15px;
+          margin-right: 10px;
+          background-color: rgb(213,192,151);
+          text-align: center;
+          color:white;
+        }
+      }
+      #askImg {
+        width: 40%;
+        position: absolute;
+        top:20px;
+        left:30%;
       }
     }
   }
